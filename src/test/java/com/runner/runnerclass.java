3c3890f5013bd051e84710a5a6d2701294a6dc36
Varahi1008 <varahi@gmail.com>
2023-07-06 16:29:18 +0530

    check

--- src/test/java/com/runner/runnerclass.java
+++ src/test/java/com/runner/runnerclass.java
@@ -2,30 +2,31 @@ package com.runner;
 
 import org.junit.BeforeClass;
 import org.junit.runner.RunWith;
-import org.openqa.selenium.WebDriver;
 
 import com.limestonenew.baseclass;
 
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
+@RunWith(Cucumber.class)
+	@CucumberOptions(features = "C:\\Users\\jayak\\eclipse-workspace\\limestone\\src\\test\\java\\com\\feature\\limestone.feature",glue="com.stepdefinition",
+	tags ="@tag",
+			
+	plugin = {"pretty","html:res/lime.html","json:res/lime.json","com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"})
 
 	
-	@RunWith(Cucumber.class)
-	@CucumberOptions(features = "C:\\Users\\jayak\\eclipse-workspace\\limestonenew\\src\\test\\java\\com\\feature\\limestone.feature", glue ="com.stepdefination",
-	
-			plugin = {"pretty", "html:Reports/lime.html", "json:Reports/lime.json"})
 
-	public class runnerclass {
-	public static WebDriver driver;
+public class runnerclass extends baseclass {
+
+
+@BeforeClass
+
+public static void Weblaunch() {
 	
-	@BeforeClass
+	baseclass.launchBrowser("chrome") ;
 	
-	public static void Weblaunch() {
-		
-		driver= baseclass.launchBrowser("chrome") ;
-		
-	}
 }
+}
+
 
 
